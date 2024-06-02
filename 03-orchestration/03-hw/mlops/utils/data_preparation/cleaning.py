cb064ec4d4aa5af322f70ec1a7a8dc7b42649f5d
barely-functional-de <qureshi.faisal712@google.com>
2024-06-02 19:56:55 +0000

    added transformer block for 03-hw

--- 03-orchestration/03-hw/mlops/utils/data_preparation/cleaning.py
+++ 03-orchestration/03-hw/mlops/utils/data_preparation/cleaning.py
@@ -22,3 +22,20 @@ def clean(
     df[categorical] = df[categorical].astype(str)
 
     return df
+
+
+def read_dataframe(dataframe):
+    df = dataframe
+
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df.duration = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    
+    return df
